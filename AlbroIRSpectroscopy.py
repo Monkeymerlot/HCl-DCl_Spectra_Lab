fd4999ff7f4ce4e9a7d8d2d70a661865f187e2cf
Joseph <37567672+Monkeymerlot@users.noreply.github.com>
2020-02-05 18:47:54 -0500

    Better plotting, filtering, and new fitting functions
    Plotting: Peak labels had a tendency to overlap. Experimented with different ways of plotting peaks including turning them on their side, tilting them at an angle, changing what side of the peak they were plotted on, and changing them to integer values. I found that plotting integer values along with centering the label above the peak worked best.
    
    Filtering: I created a new filtering algorithm. Essentially I found all of the peaks by looking only 1.005* the mean of all the points in this part of the spectrum, and this provided all of the peaks, for HCl-35 and HCl-37. I then checked all the peaks to see if they were smaller than the peaks on either side of them.
    
    Fitting: Created functions that will fit the values to the quantum numbers associated with their transition. The functions vary depending on P-Branch or R-Branch, and whether or not they are the fundamental branch or just the first overtone.

--- AlbroIRSpectroscopy.py
+++ AlbroIRSpectroscopy.py
@@ -27,8 +27,8 @@ def readSpectrum(filename):
 def plotfull(absorption, wavenumber):
     """Plot the full ro-vibrational spectrum of HCl/DCl.
     
-    This function is useful to see where the different individual spectra lie, so you can then crop
-    and plot them using "detectpeaks".
+    This function is useful to see where the different individual 
+    spectra lie, so you can then crop and plot them using "detectpeaks".
     
     Parameters
     -------
@@ -43,44 +43,60 @@ def plotfull(absorption, wavenumber):
     plt.ylabel("Relative Absorption")
 
 
-def detectpeaks(absorption, wavenumber, height, distance):
-    """Detect and label peaks based on their quantum numbers and series for HCl/DCl IR ro-vibrational
-    spectroscopy data.
+def detectpeaks(absorption, wavenumber, filter_peaks = True, height = None, distance = 40):
+    """Detect and label peaks based on their quantum numbers and series
+    for HCl/DCl IR ro-vibrational spectroscopy data.
     
-    This function takes 2 one-dimensional arrays of the absorption data and the wavenumber data for a 
-    single transition spectrum, the minimum height of the peaks, and the minimum distance between two 
-    peaks, and then will label the peaks with wavenumber, quantum number, and what branch they belong 
-    to (P-Branch or R-Branch). 
+    This function takes 2 one-dimensional arrays of the absorption data
+    and the wavenumber data for a single transition spectrum, the 
+    minimum height of the peaks, and the minimum distance between two 
+    peaks, and then will label the peaks with wavenumber, quantum 
+    number, and what branch they belong to (P-Branch or R-Branch). 
     
     Parameters
     ----------
     absorption: one-dimensional array
-        Absorption signal for one transition (eg HCl: v = 0 -> 1). Should be the minimum length and 
-        correctly trimmed. This function will not work if there is more than 1 spectrum.
+        Absorption signal for one transition (eg HCl: v = 0 -> 1). 
+        Should be the minimum length and correctly trimmed. This 
+        function will not work if there is more than 1 spectrum.
     wavenumber: one-dimensional array
-        Corresponding wavenumbers to absorption data. Must be the same length as absorption.
+        Corresponding wavenumbers to absorption data. Must be the same
+        length as absorption.
+    filter_peaks: Boolean
+        Filter peaks will detect all peaks, both HCl-35 and HCl-37, and
+        then filter out peaks which are smaller than the peaks on either
+        side of it. Peaks on the edges (largest j values in either the
+        P-Branch or R-Branch respectively) are not filtered nor checked.
     height: number
-        The minimal height that an absorption peak must have for detection by this function. Height 
-        and distance are the two ways that allow for only detecting signals created by Cl-35 instead
-        of signals created by Cl-37.
+        The minimal height that an absorption peak must have for 
+        detection by this function. Height and distance are the two ways
+        that allow for only detecting signals created by Cl-35 instead
+        of signals created by Cl-37. Must be provided if 
+        filter_peaks = False, otherwise will be ignored and automatic 
+        filtering used.
     distance: number
-        The minimal distance between neighboring absorption peaks. See height for more information.
+        The minimal distance between neighboring absorption peaks. See 
+        height for more information.
         
     Returns
     -------
     rbranch: list
-        List of the wavenumbers where absorption peaks occur for the R-Branch.
+        List of the wavenumbers where absorption peaks occur for the 
+        R-Branch.
     jr: list
-        The corresponding quantum numbers for the transitions in rbranch.
+        The corresponding quantum numbers for the transitions in 
+        rbranch.
     pbranch: list
-        List of the wavenumbers where absorption peaks occur for the P-Branch.
+        List of the wavenumbers where absorption peaks occur for the 
+        P-Branch.
     jp: list
-        The corresponding quantum numbers for the transitions in pbranch.
+        The corresponding quantum numbers for the transitions in 
+        pbranch.
         
     Warnings
     --------
-    This function must have absorption and wavenumber arrays provided. Otherwise assertion error 
-    will be raised.
+    This function must have absorption and wavenumber arrays provided.
+        Otherwise assertion error will be raised.
     Absorption and wavenumber arrays must be the same length.
     
     """
@@ -93,14 +109,31 @@ def detectpeaks(absorption, wavenumber, height, distance):
     absrange = np.ptp(absorption)
     
     #automatic ranges for x, y axis.
-    plt.ylim(np.min(absorption) - 0.2*absrange,  np.max(absorption) + 0.2*absrange)
+    plt.ylim(np.min(absorption) - 0.2*absrange,  
+             np.max(absorption) + 0.2*absrange)
     plt.xlim(np.max(wavenumber), np.min(wavenumber))
     
     plt.xlabel("$\\tilde{\\nu}$ $cm^{-1}$")
     plt.ylabel("Relative Absorption")
+
     
-    #find peaks, using height and distance.
-    peaks, _ = signal.find_peaks(absorption, height = height, distance = distance)
+    if filter_peaks == False and height != None:
+        #find peaks, using height and distance.
+        peaks, _ = signal.find_peaks(absorption, height = height, distance = distance)
+    else:
+        if filter_peaks == False and height == None:
+            print("Missing Filter Parameter: height not specified.")
+            print("Using Automatic Filtering method instead.")
+        #find all peaks (Cl35 and Cl37) and then filter out Cl37 peaks
+        peaks, _ = signal.find_peaks(absorption, height = 1.005*np.mean(absorption), prominence = 0.01)
+        filt_peaks = []
+        for i in range(1, len(peaks)-1):
+            peak = peaks[i]
+            prev_peak = peaks[i-1]
+            next_peak = peaks[i+1]
+            if absorption[peak] < absorption[prev_peak] and absorption[peak] < absorption[next_peak]:
+                filt_peaks.append(i)
+        peaks = np.delete(peaks, filt_peaks)
     
     #want to separate out the P and R branches, largest spacing is in the middle
     spacing = np.abs(peaks[:-1]-peaks[1:])
@@ -114,12 +147,12 @@ def detectpeaks(absorption, wavenumber, height, distance):
     for peak in peaks:
         if wavenumber[peak] > split:
             rbranch.append(wavenumber[peak])
-            plt.annotate(str(wavenumber[peak]), (wavenumber[peak], absorption[peak]),
-                horizontalalignment='right')
+            plt.annotate(str(int(wavenumber[peak])), (wavenumber[peak], absorption[peak]),
+                        horizontalalignment='center')
         else:
             pbranch.append(wavenumber[peak])
-            plt.annotate(str(wavenumber[peak]), (wavenumber[peak], absorption[peak]),
-                horizontalalignment='left')
+            plt.annotate(str(int(wavenumber[peak])), (wavenumber[peak], absorption[peak]),
+                        horizontalalignment='center')
         plt.axvline(wavenumber[peak], color = "gainsboro")
         
     #create arrays for quantum numbers
@@ -146,10 +179,12 @@ def detectpeaks(absorption, wavenumber, height, distance):
 
 
 def maketitle(compound, initial, final):
-    """Quick helper function to generate a title for HCl/DCl IR ro-vibrational spectroscopy data.
+    """Quick helper function to generate a title for HCl/DCl IR 
+    ro-vibrational spectroscopy data.
     
-    Takes the compound name, the initial energy level, and the final energy level and generates a 
-    properly formatted LaTeX title for a graph.
+    Takes the compound name, the initial energy level, and the final
+    energy level and generates a properly formatted LaTeX title for a
+    graph.
     
     Parameters
     ----------
@@ -165,7 +200,161 @@ def maketitle(compound, initial, final):
 
 
 def waveindex(value):
-    """converts a wavenumber to index. Assuming there are 6000 cm^-1 and 8 points/cm^-1
+    """converts a wavenumber to index. Assuming there are 6000 cm^-1 and
+    8 points/cm^-1.
     """
     index = int((6000-value)*8)
     return index
+
+
+def fit_p_branch(pbranch, jp, fundamental = False):
+    """Fits a specified function to P-Branch transition wavenumbers to
+    their corresponding quantum numbers. Also will plot function and
+    raw data.
+    
+    The specified function is 
+    nu = c -(2*b - 3*a) * j - 2*a * j**2 + 4*d * j**3 for the overtone 
+    transition, where c = 2 * ve - 6 * ve * xe, or 
+    nu = c - (2*b - 2*a) * j - a * j**2 + 4*d * j**3 for the fundamental
+    transition, where c = ve - 2 * ve * xe. 
+    
+    Parameters
+    ----------
+    pbranch: list
+        List of the wavenumbers where absorption peaks occur for the 
+        P-Branch.
+    jp: list
+        The corresponding quantum numbers for the transitions in 
+        pbranch.
+    fundamental: Boolean
+        Used to determine fitting function to use. True will use the
+        function for the fundamental transitions, while False will use
+        the function for the first overtone transitions.
+        
+    Returns
+    -------
+    a: Float
+        Rotational-vibrational coupling constant. Usually denoted a_e.
+    b: Float
+        Rotational constant. Usually denoted B_e
+    c: Float
+        Intercept of the function. For the fundemental transition this 
+        value is equal to c = ve - 2 * vexe, where ve is the harmonic
+        frequency and vexe is the anharmonicity constant.
+    d: Float
+        Centrifugal Distortion Constant, not to be confused with D_o, 
+        which is the Bond Energy.
+    pcov: list
+        List of corresponding errors at one standard deviation. Found by
+        taking the square root of the diagonal of the covariant matrix.
+    """
+    #choose function to fit to depending on transition
+    if fundamental == False:
+        def f(j, c, b, a, d):
+            nu = c -(2*b - 3*a) * j - 2*a * j**2 + 4*d * j**3
+            return nu
+    if fundamental == True:
+        def f(j, c, b, a, d):
+            nu = c - (2*b - 2*a) * j - a * j**2 + 4*d * j**3
+            return nu
+        
+    #fit the data to the function.
+    fit, pcov = optimize.curve_fit(f, jp, pbranch)
+    
+    #find the standard error from the covariance matrix
+    pcov = np.sqrt(np.diag(pcov))
+    c_err, b_err, a_err, d_err = pcov
+    #reorder errors to match returned values
+    pcov = [a_err, b_err, c_err, d_err]
+    
+    #unpack the data returned by the fitting function.
+    c, b, a, d = fit
+    
+    #create an array of values for Jp fitted function.
+    jp_int = np.linspace(jp[0], jp[-1], 100)
+    
+    #plot fitted function, and raw values
+    plt.plot(jp, pbranch, "b.")
+    plt.plot(jp_int, f(jp_int, c, b, a, d))
+    
+    plt.title("P-Branch Transitions")
+    plt.ylabel("$\\tilde{\\nu}$ $cm^{-1}$")
+    plt.xlabel("$J_P$")
+    
+    return a, b, c, d, pcov
+
+
+def fit_r_branch(rbranch, jr, fundamental = False):
+    """Fits a specified function to R-Branch transition wavenumbers to
+    their corresponding quantum numbers. Also will plot function and 
+    raw data.
+    
+    The specified function is 
+    nu = c+(2*b-5*a-4*d) + (2*b-7*a-12*d)*j - (2*a+12*d)*j**2 - 4*d*j**3
+    for the overtone transition, where c = 2*ve - 6*vexe, or 
+    nu = c+(2*b-3*a-4*d) + (2*b-4*a-12*d)*j - (a+12*d)*j**2 - 4*d*j**3 
+    for the fundamental transition, where c = ve - 2*vexe. 
+    
+    Parameters
+    ----------
+    rbranch: list
+        List of the wavenumbers where absorption peaks occur for the 
+        R-Branch.
+    jr: list
+        The corresponding quantum numbers for the transitions in 
+        rbranch.
+    fundamental: Boolean
+        Used to determine fitting function to use. True will use the
+        function for the fundamental transitions, while False will use
+        the function for the first overtone transitions.
+        
+    Returns
+    -------
+    a: Float
+        Rotational-vibrational coupling constant. Usually denoted a_e.
+    b: Float
+        Rotational constant. Usually denoted B_e
+    c: Float
+        Intercept of the function. For the fundemental transition this 
+        value is equal to c = ve - 2 * vexe, where ve is the harmonic
+        frequency and vexe is the anharmonicity constant.
+    d: Float
+        Centrifugal Distortion Constant, not to be confused with D_o, 
+        which is the Bond Energy.
+    pcov: list
+        List of corresponding errors at one standard deviation. Found by
+        taking the square root of the diagonal of the covariant matrix.
+    """
+    #choose function to fit to depending on transition
+    if fundamental == False:
+        def f(j, c, b, a, d):
+            nu = c + (2*b - 5*a - 4*d) + (2*b - 7*a - 12*d) * j - (2*a + 12*d) * j**2 - 4*d * j**3
+            return nu
+    if fundamental == True:
+        def f(j, c, b, a, d):
+            nu = c + (2*b - 3*a - 4*d) + (2*b - 4*a - 12*d) * j - (a + 12*d) * j**2 - 4*d * j**3
+            return nu
+    
+    #fit the data to the function.
+    fit, pcov = optimize.curve_fit(f, jr, rbranch)
+    #find the standard error from the covariance matrix
+    pcov = np.sqrt(np.diag(pcov))
+    c_err, b_err, a_err, d_err = pcov
+    #reorder errors to match returned values
+    pcov = [a_err, b_err, c_err, d_err]
+    
+    #unpack the data returned by the fitting function.
+    c, b, a, d = fit
+
+    #create an array of values for Jp fitted function.
+    jr_int = np.linspace(jr[0], jr[-1], 100)
+    
+    #plot fitted function, and raw values
+    plt.plot(jr, rbranch, "b.")
+    plt.plot(jr_int, f(jr_int, c, b, a, d))
+
+    plt.title("R-Branch Transitions")
+    plt.ylabel("$\\tilde{\\nu}$ $cm^{-1}$")
+    plt.xlabel("$J_R$")
+    
+    return a, b, c, d, pcov
--- Example.py
+++ Example.py
@@ -1,35 +1,88 @@
-import AlbroIRSpectroscopy
+from AlbroIRSpectroscopy import *
 
-absorb, waveln = readSpectrum("AlbroFordham.csv")   
+# Read file into the program
+absorb, waveln = readSpectrum("AlbroFordham.csv")
 
+# Plot full spectrum
 plt.figure(figsize = (20, 4))
 plotfull(absorb, waveln)
 plt.show()
 
+# Trim full spectrum down to a single transition:
 peak1a = absorb[waveindex(5850):waveindex(5450)]
 peak1w = waveln[waveindex(5850):waveindex(5450)]
+
+# Plot and label spectrum of a single transition:
 plt.figure(figsize = (20, 4))
-rb1, jr1, pb1, jp1 = detectpeaks(peak1a, peak1w, height = 0.432, distance = 40)
+rb1, jr1, pb1, jp1 = detectpeaks(peak1a, peak1w)
 maketitle("HCl", 0, 2)
 plt.show()
 
+# Fit Funciton to Wavenumber vs Quantum Number
+plt.figure(figsize = (20, 4))
+plt.subplot(121)
+ar1, br1, cr1, dr1, rerr1 = fit_r_branch(rb1, jr1, False)
+plt.subplot(122)
+ap1, bp1, cp1, dp1, perr1 = fit_p_branch(pb1, jp1, False)
+plt.show()
+
+# Print Out Fitting Parameters
+print("a_e(P-Branch):      ", ap1, "\na_e(R-Branch):      ", ar1)
+print("B_e(P-Branch):      ", bp1, "\nB_e(R-Branch):      ", br1)
+print("2ve-6vexe(P-Branch):", cp1, "\n2ve-6vexe(R-Branch):", cr1)
+print("D(P-Branch):        ", dp1, "\nD(R-Branch):        ", dr1)
+
+## Plotted, labelled and fit spectra for other transitions:
 peak3a = absorb[waveindex(3100):waveindex(2590)]
 peak3w = waveln[waveindex(3100):waveindex(2590)]
 plt.figure(figsize = (20, 4))
-rb3, jr3, pb3, jp3 = detectpeaks(peak3a, peak3w, height = 0.55, distance = 40)
+rb3, jr3, pb3, jp3 = detectpeaks(peak3a, peak3w)
 maketitle("HCl", 0, 1)
+plt.show()
+
+plt.figure(figsize = (20, 4))
+plt.subplot(121)
+ar3, br3, cr3, dr3, rerr3 = fit_r_branch(rb3, jr3, True)
+plt.subplot(122)
+ap3, bp3, cp3, dp3, perr3 = fit_p_branch(pb3, jp3, True)
 plt.show()
 
 peak2a = absorb[waveindex(4240):waveindex(3990)]
 peak2w = waveln[waveindex(4240):waveindex(3990)]
 plt.figure(figsize = (20, 4))
-rb2, jr2, pb2, jp2 = detectpeaks(peak2a, peak2w, height = 0.46, distance = 40)
+rb2, jr2, pb2, jp2 = detectpeaks(peak2a, peak2w)
 maketitle("DCl", 0, 2)
 plt.show()
+
+plt.figure(figsize = (20, 4))
+plt.subplot(121)
+ar2, br2, cr2, dr2, rerr2 = fit_r_branch(rb2, jr2, False)
+plt.subplot(122)
+ap2, bp2, cp2, dp2, perr2 = fit_p_branch(pb2, jp2, False)
+plt.show()
+
 
 peak4a = absorb[waveindex(2250):waveindex(1875)]
 peak4w = waveln[waveindex(2250):waveindex(1875)]
 plt.figure(figsize = (20, 4))
-rb4, jr4, pb4, jp4 = detectpeaks(peak4a, peak4w, height = 0.7, distance = 40)
+rb4, jr4, pb4, jp4 = detectpeaks(peak4a, peak4w)
 maketitle("DCl", 0, 1)
 plt.show()
+
+plt.figure(figsize = (20, 4))
+plt.subplot(121)
+ar4, br4, cr4, dr4, rerr4 = fit_r_branch(rb4, jr4, True)
+plt.subplot(122)
+ap4, bp4, cp4, dp4, perr4 = fit_p_branch(pb4, jp4, True)
+plt.show()
+
+# Print out all fitting parameters:
+# Uncomment to print all values...
+#print(ar1, ar3, ar2, ar4)
+#print(ap1, ap3, ap2, ap4)
+#print(br1, br3, br2, br4)
+#print(bp1, bp3, bp2, bp4)
+#print(cr1, cr3, cr2, cr4)
+#print(cp1, cp3, cp2, cp4)
+#print(dr1, dr3, dr2, dr4)
+#print(dp1, dp3, dp2, dp4)
